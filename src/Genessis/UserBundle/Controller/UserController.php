b63f41b02ba52b489df341a5032309a83e439ec6
Genessis Jimenez <Genesis.jz.93@gmail.com>
2016-07-31 21:39:06 -0400

    Editar user implementado - Modificación del not_blank del password field en User entity

--- src/Genessis/UserBundle/Controller/UserController.php
+++ src/Genessis/UserBundle/Controller/UserController.php
@@ -7,6 +7,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Genessis\UserBundle\Entity\User;
 use Genessis\UserBundle\Form\UserType;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Validator\Constraints as Assert;
+
 
 class UserController extends Controller
 {
@@ -52,22 +55,97 @@ class UserController extends Controller
     	if($form->isValid()){
 
     		$password = $form->get('password')->getData();
-    		$encoder = $this->container->get('security.password_encoder');
-    		$encoded = $encoder->encodePassword($user, $password);
 
-    		$user->setPassword($encoded);
+    		$passwordConstraint = new Assert\NotBlank();
+    		$errorList = $this->get('validator')->validate($password, $passwordConstraint);
+    		if(count($errorList)==0){
+    			$encoder = $this->container->get('security.password_encoder');
+	    		$encoded = $encoder->encodePassword($user, $password);
 
-    		$em = $this->getDoctrine()->getManager();
-    		$em->persist($user);
-    		$em->flush();
+	    		$user->setPassword($encoded);
 
-    		$successMessage = $this->get('translator')->trans('The user has been created.');
-    		$this->addFlash('mensaje', $successMessage);
+	    		$em = $this->getDoctrine()->getManager();
+	    		$em->persist($user);
+	    		$em->flush();
 
-    		return $this->redirectToRoute('genessis_user_index');
+	    		$successMessage = $this->get('translator')->trans('The user has been created.');
+	    		$this->addFlash('mensaje', $successMessage);
+
+	    		return $this->redirectToRoute('genessis_user_index');
+    		}else{
+    			$errorMessage = new FormError($errorList[0]->getMessage());
+    			$form->get('password')->addError($errorMessage);
+    		}
     	}
     	return $this->render('GenessisUserBundle:User:add.html.twig', array('form'=>$form->createView()));
-    } 
+    }
+
+    public function editAction($id){
+    	$em = $this->getDoctrine()->getManager();
+    	$user = $em->getRepository('GenessisUserBundle:User')->find($id);
+
+    	if(!$user){
+    		$messageException = $this->get('translator')->trans('User not found.');
+    		throw $this->createNotFoundException($messageException);
+    	}
+
+    	$form = $this->createEditForm($user);
+
+    	return $this->render('GenessisUserBundle:User:edit.html.twig', array('user'=>$user, 'form'=>$form->createView()));
+    }
+
+    private function createEditForm(User $entity){
+    	$form = $this->createForm(UserType::class, $entity, array('action'=>$this->generateUrl('genessis_user_update', array('id'=>$entity->getId())), 'method'=>'PUT'));
+    	return $form;
+    }
+
+    public function updateAction($id, Request $request){
+    	$em = $this->getDoctrine()->getManager();
+    	$user = $em->getRepository('GenessisUserBundle:User')->find($id);
+
+    	if(!$user){
+    		$messageException = $this->get('translator')->trans('User not found.');
+    		throw $this->createNotFoundException($messageException);
+    	}
+
+    	$form = $this->createEditForm($user);
+    	$form->handleRequest($request);
+
+    	if($form->isSubmitted() && $form->isValid()){
+
+    		$password = $form->get('password')->getData();
+    		if(!empty($password)){
+    			$encoder = $this->container->get('security.password_encoder');
+    			$encoded = $encoder->encodePassword($user, $password);
+    			$user->setPassword($encoded);
+    		}else{
+    			$recoverPass = $this->recoverPass($id);
+    			$user->setPassword($recoverPass[0]['password']);
+    		}
+
+    		if($form->get('role')->getData() == 'ROLE_ADMIN'){
+    			$user->setIsActive(1);
+    		}
+
+    		$em->flush();
+    		$successMessage = $this->get('translator')->trans('The user has been modified.');
+    		$this->addFlash('mensaje', $successMessage);
+
+    		return $this->redirectToRoute('genessis_user_edit', array('id'=>$user->getId()));
+    	}
+    	return $this->render('GenessisUserBundle:User:edit.html.twig', array('user'=>$user, 'form'=>$form->createView()));
+    }
+
+    private function recoverPass($id){
+    	$em = $this->getDoctrine()->getManager();
+    	$query = $em->createQuery(
+    		'SELECT u.password
+    		FROM GenessisUserBundle:User u
+    		WHERE u.id = :id'
+    	)->setParameter('id', $id);
+    	$currentPass = $query->getResult();
+    	return $currentPass;
+    }
 
     public function viewAction($id){
     	$repository = $this->getDoctrine()->getRepository('GenessisUserBundle:User');
